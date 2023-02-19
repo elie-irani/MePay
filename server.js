8e7afbb82a1fca14214c10a90acdfcd901b9330a
Elie <elie.3irani@gmail.com>
2023-02-19 12:21:24 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+require('dotenv').config();
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(process.env.APP_PORT, process.env.APP_URL, () => {
+  console.log(process.env);
+  console.log(`Server running at http://${process.env.APP_URL}:${process.env.APP_PORT}/`);
+});
